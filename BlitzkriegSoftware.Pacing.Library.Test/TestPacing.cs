c5c952f5546871c244836ce69fd76b040df3701c
Stuart Williams <spookdejur@hotmail.com>
2020-09-02 14:29:30 -0700

    Working!

--- BlitzkriegSoftware.Pacing.Library.Test/TestPacing.cs
+++ BlitzkriegSoftware.Pacing.Library.Test/TestPacing.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BlitzkriegSoftware.Pacing.Library.Test
@@ -18,11 +19,46 @@ namespace BlitzkriegSoftware.Pacing.Library.Test
 
         #endregion
 
+        public const string KeyPrefix = "test";
+        public const string KeySuffix = "Pace1";
 
         [TestMethod]
         [TestCategory("Integration")]
         public void TestPacer1()
         {
+            var agent = new PacerAgent(new Models.RedisConfiguration(), KeyPrefix);
+
+            Assert.IsTrue(agent.IsValid);
+
+            var ts = new TimeSpan(0, 0, 0, 1, 0);
+
+            agent.MarkPacing(KeySuffix, ts);
+
+            var sleepTS = ts.Add(new TimeSpan(0, 0, 0, 5));
+
+            System.Threading.Thread.Sleep(sleepTS);
+
+            var runnable = agent.Runnable(KeySuffix);
+
+            Assert.IsTrue(runnable);
         }
+
+        [TestMethod]
+        [TestCategory("Integration")]
+        public void TestPacer2()
+        {
+            var agent = new PacerAgent(new Models.RedisConfiguration(), KeyPrefix);
+
+            Assert.IsTrue(agent.IsValid);
+
+            var ts = new TimeSpan(0, 0, 0, 1, 0);
+
+            agent.MarkPacing(KeySuffix, ts);
+
+            var runnable = agent.Runnable(KeySuffix);
+
+            Assert.IsFalse(runnable);
+        }
+
     }
 }
--- BlitzkriegSoftware.Pacing.Library/PacerAgent.cs
+++ BlitzkriegSoftware.Pacing.Library/PacerAgent.cs
@@ -4,5 +4,98 @@ namespace BlitzkriegSoftware.Pacing.Library
 {
     public class PacerAgent
     {
+        #region "Fields"
+        
+        private Models.RedisConfiguration _redisconfig;
+        private string _keyprefix;
+        private BlitzRedisClient _client;
+        #endregion
+
+        #region "CTOR"
+
+        private PacerAgent() { }
+
+        /// <summary>
+        /// CTOR
+        /// </summary>
+        /// <param name="redisConfiguration"></param>
+        /// <param name="keyPrefix"></param>
+        public PacerAgent(Models.RedisConfiguration redisConfiguration, string keyPrefix)
+        {
+            _redisconfig = redisConfiguration;
+            _client = new BlitzRedisClient(_redisconfig);
+            _keyprefix = keyPrefix;
+        }
+
+        #endregion
+
+        #region "Properties"
+
+        /// <summary>
+        /// Is a Valid Pacer Agent
+        /// </summary>
+        public bool IsValid
+        {
+            get
+            {
+                return _redisconfig.IsValid && !string.IsNullOrWhiteSpace(this._keyprefix);
+            }
+        }
+
+        /// <summary>
+        /// Make Redis Key
+        /// </summary>
+        /// <param name="keySuffix"></param>
+        /// <returns></returns>
+        private string MakeRedisKey(string keySuffix)
+        {
+            return $"{this._keyprefix}-{keySuffix}";
+        }
+
+        #endregion
+
+        #region "Methods"
+
+        /// <summary>
+        /// Runnable
+        /// </summary>
+        /// <param name="keySuffix">Suffix</param>
+        /// <returns>Fully formed key</returns>
+        public bool Runnable(string keySuffix)
+        {
+            bool isRunnable = false;
+
+            if (string.IsNullOrWhiteSpace(keySuffix)) throw new ArgumentNullException(nameof(keySuffix));
+
+            var key = MakeRedisKey(keySuffix);
+            var item = _client.Get<DateTime>(key);
+
+            if (item < DateTime.UtcNow) isRunnable = true;
+
+            return isRunnable;
+        }
+
+        /// <summary>
+        /// MarkPacing
+        /// </summary>
+        /// <param name="keySuffix">Suffix</param>
+        /// <param name="fromNow">TimeSpan</param>
+        public void MarkPacing(string keySuffix, TimeSpan fromNow)
+        {
+            if (string.IsNullOrWhiteSpace(this._keyprefix)) throw new ArgumentNullException(nameof(this._keyprefix));
+            
+            if (string.IsNullOrWhiteSpace(keySuffix)) throw new ArgumentNullException(nameof(keySuffix));
+
+            if (fromNow.TotalMilliseconds <= 0.0d) throw new ArgumentNullException(nameof(fromNow));
+
+            var key = MakeRedisKey(keySuffix);
+            var dt = DateTime.UtcNow.Add(fromNow);
+
+            _client.Set<DateTime>(key, dt);
+        }
+
+        #endregion
+
+
     }
 }
